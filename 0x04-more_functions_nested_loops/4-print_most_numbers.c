63c4bc72ed6d61cc10e47fd77f8bbd2214863a9b
Blarchi <lykababe3@gmail.com>
2022-12-23 06:56:08 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,6 +5,7 @@
 /**
  * print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
  *
+ * printfflush/_putchar
  * Return: nothing
  */
 void print_most_numbers(void)
@@ -16,5 +17,8 @@ void print_most_numbers(void)
 		if (!(number == 2) || (number == 4))
 			_putchar(number);
 	}
-	_putchar(10);
+	/**printf(“retrans\n”)
+        *printfflush/_putchar
+	*/
+	_putchar('\n');
 }
