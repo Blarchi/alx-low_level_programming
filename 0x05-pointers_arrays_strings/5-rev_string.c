c32c8a47195259cad12abdbc23e1c9e9507c5975
Blarchi <lykababe3@gmail.com>
2023-01-25 05:12:37 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string _ reverses string
+ * @s: inputs
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int b = 0;
+	int i;
+
+	while (s[b] != '\0')
+		b++;
+	for (i = 0; i < b; i++)
+	{
+		b--;
+		rv = s[i];
+		s[i] = s[b];
+		s[b] = rv;
+	}
+}
