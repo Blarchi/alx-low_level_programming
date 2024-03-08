d7f0afef78961b988834b08f0d0a4f6db6a63516
Blarchi <you@example.com>
2024-03-07 20:49:46 -0800

    102-print_comb5

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,24 +9,23 @@
 
 int main(void)
 {
-    int i, j;
-
-    for (i = '0'; i < '9'; i++)
-    {
-
-            for (j = i + 1; j <= '9'; j++)
-            {
-                    if (j != i)
-                    {
-                            putchar(i);
-                            putchar(j);
-                            if (i == '8' && j == '9')
-                                    continue;
-                            putchar(',');
-                            putchar(' ');
-                    }
-            }
-    }
-    putchar('\n');
-    return (0);
+	int i, j;
+	for (i = '0'; i < '9'; i++)
+	{
+		
+		for (j = i + 1; j <= '9'; j++)
+		{
+			if (j != i)
+			{
+				putchar(i);
+				putchar (j);
+				if (i == '8' && j == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,7 +25,7 @@ int main(void)
 					putchar (y);
 					if (r == '7' && y == '8')
 						continue;
-					putchar ('.');
+					putchar (',');
 					putchar(' ');
 				}
 			}
