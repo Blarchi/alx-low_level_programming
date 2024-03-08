74e85b9ed4bc3dd7f56dd93b694cd50355dd9ed4
Blarchi <you@example.com>
2024-03-07 20:33:24 -0800

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int a, n;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (n = a + 1; n <= 99; n++)
+		{
+			putchar ((a/ 10) + '0');
+			putchar ((a % 10) + '0');
+			putchar (' ');
+			putchar ((n / 10) + '0');
+			putchar ((n % 10) + '0');
+			if (a == 98 && n == 99)
+				continue;
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
+
