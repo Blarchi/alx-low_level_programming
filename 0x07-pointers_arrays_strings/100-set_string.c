24e0f5ad028b8bce01bf212595a8b0322a4111b8
Blarchi <you@example.com>
2024-03-10 14:22:23 -0700

    101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - check description
+ * Description: function that sets the value of a
+ * pointer to a char
+ * @s:address
+ * @to:address
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
