8539d7a3b4c5ff03485f6b5d571dc5f3a1e30e9f
Blarchi <you@example.com>
2024-03-10 12:31:27 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,51 @@
 #include "main.h"
-
+#include <unistd.h>
 /**
- * print_times_table - Prints times table of the input,
- * starting with 0.
- * @r: The value of the times table to be printed.
+ * print_times_table - print
+ * Description: prints the n times table
+ * Return: 0
+ * @n:input
  */
-
-void print_times_table(int r)
-
+void print_times_table(int n)
 {
-	int num, mul, pro;
-	if (r >= 0 && r <= 15)
-	{
-		for (num = 0; num <= r; num++)
-		{
-			_putchar('0');
-			for (mul = 1; mul <= r; mul++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				pro = num * mul;
-				if (pro <= 99)
-					_putchar(' ');
-				if (pro <= 9)
-				_putchar(' ');
-				if (pro >= 100)
-				{
-					_putchar((pro / 100) + '0');
-					_putchar(((pro /10)) % 10 + '0');
-				}
-				else if (pro <= 99 && pro >= 10)
-				{
-					_putchar((pro / 10) + '0');
-				}
-				_putchar((pro / 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+int a;
+int b;
+if (n >= 0 && n <= 15)
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+if ((a * b) < 10)
+{
+if (b != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar((b * a) + '0');
+}
+else if ((a * b) < 100)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((((b * a) / 10) % 10) + '0');
+_putchar(((b * a) % 10) + '0');
+}
+else if ((a * b) < 1000)
+{
+_putchar(' ');
+_putchar((((b * a) / 100) % 10) + '0');
+_putchar((((b * a) / 10) % 10) + '0');
+_putchar(((b * a) % 10) + '0');
+}
+if (b != (n))
+{
+_putchar(',');
+}
+}
+_putchar('\n');
+}
+}
 }
